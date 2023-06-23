7fa87e42608d4427405ff9c29f41a9ef30dab96b
zobaidul kazi <mdzobaidulislamkazi@gmail.com>
2023-06-23 18:14:55 +0600

    kazibyte_development app up

---
+++ app/api/blog/route.ts
@@ -0,0 +1,8 @@
+export const GET = async (req: Request, res: Response) => {
+    console.log('Get App')
+}
+
+
+export const POST = async (req: Request, res: Response) => {
+    console.log('POST MAthod')
+}

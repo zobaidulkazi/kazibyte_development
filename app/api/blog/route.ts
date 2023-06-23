a8522af3c9c54f80fffcd5fa87c74c72d439fc13
zobaidul kazi <mdzobaidulislamkazi@gmail.com>
2023-06-23 18:26:04 +0600

    add about page and

--- app/api/blog/route.ts
+++
@@ -1,8 +0,0 @@
-export const GET = async (req: Request, res: Response) => {
-    console.log('Get App')
-}
-
-
-export const POST = async (req: Request, res: Response) => {
-    console.log('POST MAthod')
-}

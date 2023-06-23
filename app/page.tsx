cb2f8ec1325ba08a67af66cf141f473131bd3a0a
zobaidul kazi <mdzobaidulislamkazi@gmail.com>
2023-06-23 18:47:46 +0600

    link target blank

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className='text-5xl'>kazibyte Development</h1>
 
-      <a href='./about' >
+      <a href='./about' target='_blank' >
         About Page
       </a>
     </main>
